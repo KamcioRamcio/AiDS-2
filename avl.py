4f99cb31872f1f8bef3079456a7edd79308a103f
KamcioRamcio <kamilecki12@gmail.com>
2024-04-21 20:36:10 +0200

    kamcio1

--- avl.py
+++ avl.py
@@ -1,4 +1,20 @@
-def avltree():
-    return AVLTree()
-
-print(avltree())
+class Node:
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
+        self.height = 1
+    def insert_node(self,key):
+        if self.key is None:
+            self.key = key
+        elif key < self.key:
+            if self.left is None:
+                self.left = Node(key)
+            else:
+                self.left.insert_node(key)
+        elif key > self.key:
+            if self.right is None:
+                self.right = Node(key)
+            else:
+                self.right.insert_node(key)
+                
