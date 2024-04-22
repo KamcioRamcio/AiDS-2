157d95fafc8223d2ec45747538d9e455035869b0
KamcioRamcio <kamilecki12@gmail.com>
2024-04-22 18:35:55 +0200

    to-main

--- avl-gpt.py
+++ avl-gpt.py
@@ -1,29 +1,3 @@
-def QuickSortLeftPivot(arr, start, end):
-    if start < end:
-        pivot_index = Partition(arr, start, end)
-        QuickSortLeftPivot(arr, start, pivot_index - 1)
-        QuickSortLeftPivot(arr, pivot_index + 1, end)
-    return arr
-def Partition(arr, start, end):
-    pivot = arr[start]
-    left = start + 1
-    right = end
-    
-    while True:
-        while left <= right and arr[left] <= pivot:
-            left += 1
-        while left <= right and arr[right] >= pivot:
-            right -= 1
-        if left > right:
-            break
-        else:
-            arr[left], arr[right] = arr[right], arr[left]
-    
-    arr[start], arr[right] = arr[right], arr[start]
-    return right
-
-
-
 
 class TreeNode:
     def __init__(self, value):
--- bst.py
+++ bst.py
@@ -1,33 +1,32 @@
-# Dałem tu Quicksort żeby posortować tablice bo do AVL trzeba posortowaną rosnoąco tablice
-#masz link żeby sobie wizułalizować jak wyglada BST które wpisujesz
+
 vizualization = "https://www.cs.usfca.edu/~galles/visualization/BST.html"
-
 import random
-
 random_array = random.sample(range(1, 1001), 10)
 my_array=[7,8,1,2,22,58,3,47,121]
 
 
 
-# No tu sobie zrobiłem drzewo binarne BST, masz ziutka value który ci jakby definiuje aktuanly węzeł, left i right to są dzieci
 class TreeNode:
-    def __init__(self, value):
+    def __init__(self, value=None):
         self.value = value
         self.left = None
         self.right = None
-# No tu sobie zrobiłem funkcje insert która dodaje węzeł do drzewa
+
     def insert(self,value):
-        if value < self.value:
-            if self.left is None:
-                self.left = TreeNode(value)
-            else:
-                self.left.insert(value)
+        if self.value is None:
+            self.value = value
         else:
-            if self.right is None:
-                self.right = TreeNode(value)
-            else:
-                self.right.insert(value)
-#Tera troche nie w kolejności ale czytam i ogladam i tak tłumaczyli to tak pisze, inna sprawa że potem po prsotu inaczej będziemy wywoływać to
+            if value < self.value:
+                if self.left is None:
+                    self.left = TreeNode(value)
+                else:
+                    self.left.insert(value)
+            else: 
+                if self.right is None:
+                    self.right = TreeNode(value)
+                else:
+                    self.right.insert(value)
+
 #in_order ---- lewy, korzeń, prawy
     def in_order(self):
         if self.left:
@@ -99,21 +98,17 @@ class TreeNode:
             self.post_order_delete(self)
             
     def post_order_delete(self, node):
-        if node is None:
-            return
-        self.post_order_delete(node.left)
-        self.post_order_delete(node.right)
-        self.delete(node.value)
-        print("Usunięto: ", node.value)
-        print(tree.post_order())
-        return 
+        if node:
+            self.post_order_delete(node.left)
+            self.post_order_delete(node.right)
+            print("Usuwam węzeł", node.value)
+            if node.left:
+                node.left = None
+            if node.right:
+                node.right = None
+            if node.value:
+                node.value = None
+            del node
+            
         
-        
-        
-
-
-tree = TreeNode(my_array[0])  
-for i in range(1, len(my_array)):  
-    tree.insert(my_array[i]) 
-
 
