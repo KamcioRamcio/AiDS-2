c7eff6ab2bbcaa4603a2c7613531e5d1c15862a7
KhalSCI <kubapp23@gmail.com>
2024-04-23 18:46:02 +0200

    Lece na customka to zapisuje co zrobilem menu dziala dla avl

--- main.py
+++ main.py
@@ -8,7 +8,96 @@ parser.add_argument("-bst", action="store_true", help="Work on BST tree")
 args = parser.parse_args()
 CURRENT_DIR = os.path.dirname(__file__)
 positions = {}
+def tikz_tree_helper(node, level=0, pos=0, positions=None):
+    if positions is None:
+        positions = {}  # Definiujemy słownik positions, jeśli nie został jeszcze zdefiniowany
+    if node is None:
+        return "", pos
+    # Check if the position is already occupied
+    while (pos, -level) in positions:
+        # If it is, shift the position to the right
+        pos += 1
+    # Record the position of the current node
+    positions[(pos, -level)] = node.value  # Poprawiamy odwołanie do wartości węzła
+    result = "\\node at ({},{}) {{{}}};\n".format(pos, -level, node.value)  # Poprawiamy odwołanie do wartości węzła
+    if node.left:
+        left_result, left_pos = tikz_tree_helper(node.left, level+1, pos-1, positions)
+        result += left_result
+        result += "\\draw ({},{}) -- ({},{});\n".format(pos, -level, left_pos, -(level+1))
+    if node.right:
+        right_result, right_pos = tikz_tree_helper(node.right, level+1, pos+1, positions)
+        result += right_result
+        result += "\\draw ({},{}) -- ({},{});\n".format(pos, -level, right_pos, -(level+1))
+    return result, pos
 
+def tikz_tree(node):
+    result, _ = tikz_tree_helper(node)
+    return result
+
+def writeTikzToFile(filename, text):
+    with open(filename, 'w') as file:
+        file.write("\\documentclass{standalone}\n")
+        file.write("\\usepackage{tikz}\n")
+        file.write("\\begin{document}\n")
+        file.write("\\begin{tikzpicture}[\n")
+        file.write("level distance=1cm,\n")
+        file.write("level 1/.style={sibling distance=3cm},\n")
+        file.write("level 2/.style={sibling distance=1.5cm},\n")
+        file.write("level 3/.style={sibling distance=1cm}\n")
+        file.write("]\n")
+    file.close()
+    with open(filename, 'a') as file:
+        file.write(text)
+    file.close()
+    with open(filename, 'a') as file:
+        file.write("\\end{tikzpicture}\n")
+        file.write("\\end{document}\n")
+        
+def tikz_tree_helper(node, level=0, pos=0, positions=None):
+    if positions is None:
+        positions = {}  # Definiujemy słownik positions, jeśli nie został jeszcze zdefiniowany
+    if node is None:
+        return "", pos
+    # Check if the position is already occupied
+    while (pos, -level) in positions:
+        # If it is, shift the position to the right
+        pos += 1
+    # Record the position of the current node
+    positions[(pos, -level)] = node.value  # Poprawiamy odwołanie do wartości węzła
+    result = "\\node at ({},{}) {{{}}};\n".format(pos, -level, node.value)  # Poprawiamy odwołanie do wartości węzła
+    if node.left:
+        left_result, left_pos = tikz_tree_helper(node.left, level+1, pos-1, positions)
+        result += left_result
+        result += "\\draw ({},{}) -- ({},{});\n".format(pos, -level, left_pos, -(level+1))
+    if node.right:
+        right_result, right_pos = tikz_tree_helper(node.right, level+1, pos+1, positions)
+        result += right_result
+        result += "\\draw ({},{}) -- ({},{});\n".format(pos, -level, right_pos, -(level+1))
+    return result, pos
+
+def tikz_tree(node):
+    result, _ = tikz_tree_helper(node)
+    return result
+
+def writeTikzToFile(filename, text):
+    with open(filename, 'w') as file:
+        file.write("\\documentclass{standalone}\n")
+        file.write("\\usepackage{tikz}\n")
+        file.write("\\begin{document}\n")
+        file.write("\\begin{tikzpicture}[\n")
+        file.write("level distance=1cm,\n")
+        file.write("level 1/.style={sibling distance=3cm},\n")
+        file.write("level 2/.style={sibling distance=1.5cm},\n")
+        file.write("level 3/.style={sibling distance=1cm}\n")
+        file.write("]\n")
+    file.close()
+    with open(filename, 'a') as file:
+        file.write(text)
+    file.close()
+    with open(filename, 'a') as file:
+        file.write("\\end{tikzpicture}\n")
+        file.write("\\end{document}\n")
+        
 def chosenTree(treeName, tree, root):
     treeName = treeName.upper()
     while True:
@@ -24,7 +113,7 @@ def chosenTree(treeName, tree, root):
             print("Rebalance - rebalance the AVL tree")
             print("Remove - remove a node from the trees")
             print("MinMax - find the minimum and maximum values in the trees")
-            print("Height- return the height of the tree")
+            print("Draw - generate a TikZ code for the tree and save it to tree.tex")
             print("-------------")
             continue
         if command == 'exit':
@@ -64,9 +153,11 @@ def chosenTree(treeName, tree, root):
             print(tree.find(znajdzka))
         elif command == 'minmax':
             print("Max: ",tree.largest(),"\nMin: ",tree.smallest(),"\n")  
-        elif command == 'height':
-            
-            print("Height: ",tree.get_height())
+        elif command == 'draw':
+             #Wygenerowanie kodu TikZ dla drzewa
+            tikz_code = tikz_tree(tree)
+            writeTikzToFile("tree.tex", tikz_code)
+            print("TikZ code has been generated and saved to tree.tex.")   
         else:
             print("Unknown command. Please try again. Type help for more information.")
 
