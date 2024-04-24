1868a93846369eed335954cee4b0584cc2ab7bf2
KamcioRamcio <kamilecki12@gmail.com>
2024-04-24 10:17:28 +0200

    dziala

--- avl.py
+++ avl.py
@@ -1,45 +1,71 @@
 class Node:
-    def __init__(self, value=None):
+    def __init__(self, value):
         self.value = value
         self.left = None
         self.right = None
         self.parent = None
-        self.height = 0
+        self.height = 1
 
 class AVL:
     def __init__(self):
         self.root = None
 
-    def insert(self, arr):
-        if len(arr) == 0:
-            return
-        arr.sort()  # Sortujemy tablicę, aby umożliwić łatwiejsze tworzenie drzewa AVL
-        self.root = self._create_balanced_avl(arr, 0, len(arr)-1)
-
-    def _create_balanced_avl(self, arr, start, end):
-        if start > end:
-            return None
-        
-        mid = (start + end) // 2
-        root = Node(arr[mid])
-
-        root.left = self._create_balanced_avl(arr, start, mid - 1)
-        if root.left:
-            root.left.parent = root
-
-        root.right = self._create_balanced_avl(arr, mid + 1, end)
-        if root.right:
-            root.right.parent = root
-
-        root.height = 1 + max(self.get_height(root.left), self.get_height(root.right))
-        return root
+    def insert(self, value):
+        self.root = self.insertNode(value, self.root)
     
+    def insertNode(self, value, node):
+        if node is None:
+            return Node(value)
+
+        if value < node.value:
+            node.left = self.insertNode(value, node.left)
+        elif value > node.value:
+            node.right = self.insertNode(value, node.right)
+
+        node.height = 1 + max(self.get_height(node.left), self.get_height(node.right))
+        
+        balance = self.get_balance(node)
+
+        # Balansowanie drzewa po wstawieniu nowego węzła
+        if balance > 1 and value < node.left.value:
+            return self.right_rotate(node)
+        
+        if balance < -1 and value > node.right.value:
+            return self.left_rotate(node)
+        
+        if balance > 1 and value > node.left.value:
+            node.left = self.left_rotate(node.left)
+            return self.right_rotate(node)
+        
+        if balance < -1 and value < node.right.value:
+            node.right = self.right_rotate(node.right)
+            return self.left_rotate(node)
+        
+        return node
+    
+    def build_tree(self, arr):
+        for i in arr:
+            self.insert(i)
+        return self.root
     
     def get_height(self, node):
         if not node:
             return 0
         return node.height
+    
+    def Biggest_Left(self, node):
+        while node.right:
+            node = node.right
+        return node
 
+    def get_balance(self, node):
+        if not node:
+            return 0
+        return self.get_height(node.left) - self.get_height(node.right)
+    
+    
+    
+    
     def in_order(self, node=None):
         if node is None:
             node = self.root
@@ -49,74 +75,74 @@ class AVL:
         print(node.value)
         if node.right:
             self.in_order(node.right)
+    def post_order(self, node=None):
+        if node is None:
+            node = self.root
+        
+        if node.left:
+            self.post_order(node.left)
+        if node.right:
+            self.post_order(node.right)
+        print(node.value)
+        
+    def pre_order(self, node=None):
+        if node is None:
+            node = self.root
+        print(node.value)
+        if node.left:
+            self.pre_order(node.left)
+        if node.right:
+            self.pre_order(node.right)
 
-    def right_rotation(self, node):
+    def right_rotate(self, node):
         print("Rotacja w prawo wokół", node.value)
         pivot = node.left
         node.left = pivot.right
         pivot.right = node
-        node.height = 1 + max(self.getHeight(node.left), self.getHeight(node.right))
-        pivot.height = 1 + max(self.getHeight(pivot.left), self.getHeight(pivot.right))
+        node.height = 1 + max(self.get_height(node.left), self.get_height(node.right))
+        pivot.height = 1 + max(self.get_height(pivot.left), self.get_height(pivot.right))
 
         return pivot
 
-    def create_vine(self):
-        root = self.root
-        while root.left:
-            self.right_rotate(root)
-            root = root.parent
-
-    def balance_from_vine(self, size):
-        vine_height = int(size ** 0.5)
-        remaining_nodes = size - (2 ** vine_height - 1)
-        self.make_vine_balanced(vine_height)
-        for _ in range(remaining_nodes):
-            if self.root:
-                self.left_rotate(self.root)
-
-    def make_vine_balanced(self, size):
-        compression_point = size // 2
-        for _ in range(compression_point):
-            self.left_rotate(self.root)
-
-    def left_rotate(self, root):
-        sub_root = root.parent
-        right_child = root.right
-        if right_child is None:
-            return
-        root.right = right_child.left
-        if right_child.left:
-            right_child.left.parent = root
-        right_child.left = root
-        root.parent = right_child
-        right_child.parent = sub_root
-        if sub_root is None:
-            self.root = right_child
-        elif sub_root.left == root:
-            sub_root.left = right_child
+    def left_rotate(self, node):
+        print("Rotacja w lewo wokół", node.value)
+        pivot = node.right
+        node.right = pivot.left
+        pivot.left = node
+        node.height = 1 + max(self.get_height(node.left), self.get_height(node.right))
+        pivot.height = 1 + max(self.get_height(pivot.left), self.get_height(pivot.right))
+        
+        return pivot
+    
+    
+    
+    def find_node(self, value, node=None):
+        if node is None:
+            node = self.root
+            
+        if node is None:
+            return None
+        
+        if value == node.value:
+            return node
+        elif value < node.value:
+            return self.find_node(value, node.left)
         else:
-            sub_root.right = right_child
-        root.height = 1 + max(self.get_height(root.left), self.get_height(root.right))
-        right_child.height = 1 + max(self.get_height(right_child.left), self.get_height(right_child.right))
+            return self.find_node(value, node.right)
+
     
-    def Biggest_Left(self, node):
-        while node.right:
-            node = node.right
-        return node
-    def get_balance(self, node):
-        if not node:
-            return 0
-        return self.get_height(node.left) - self.get_height(node.right)
+    def delete(self, value, node=None):
+        if node is None:
+            node = self.root
+        self.root = self.deleteNode(value, node)
     
-    
-    
-    def delete(self, value , node):
+    def deleteNode(self, value, node):
         if node is None:
             return None
         if value < node.value:
-            node.left = self.delete(value, node.left)
+            node.left = self.deleteNode(value, node.left)
         elif value > node.value:
-            node.right = self.delete(value, node.right)
+            node.right = self.deleteNode(value, node.right)
         else:
             if not node.left and not node.right:
                 del node
@@ -129,10 +155,9 @@ class AVL:
                 temp = node.left
                 del node
                 return temp
-            #-- Jeśli węzeł ma obu potomków to szukamy największego elementu w lewym poddrzewie --#
             temp = self.Biggest_Left(node.left)
             node.value = temp.value
-            node.left = self.delete(temp.value, node.left)
+            node.left = self.deleteNode(temp.value, node.left)
         if not node:
             return node
         node.height = 1 + max(self.get_height(node.left), self.get_height(node.right))
@@ -155,16 +180,30 @@ class AVL:
             return self.left_rotate(node)
         
         return node
-	
+
+    def delete_all(self):
+        self.post_order_delete(self.root)
+        
+    def post_order_delete(self, node):
+        if node:
+            self.post_order_delete(node.left)
+            self.post_order_delete(node.right)
+            print("Usuwam węzeł", node.value)
+            if node.left:
+                node.left = None
+            if node.right:
+                node.right = None
+            if node.value:
+                node.value = None
+            del node
 
 
+#avl = AVL()
+#avl.build_tree([7, 1, 2, 22, 58, 3, 47, 121, 12])
+#avl.in_order()
+#
+#avl.in_order()
 
-avl = AVL()
-avl.insert([7, 1, 2, 22, 58, 3, 47, 121,12])
-avl.in_order()
-
-avl.delete(22, avl.root)
-avl.in_order()
 
 def tikz_tree_helper(node, level=0, pos=0, positions=None):
     if positions is None:
@@ -212,8 +251,9 @@ def writeTikzToFile(filename, text):
         
 
 #Wygenerowanie kodu TikZ dla drzewa
-tikz_code = tikz_tree(avl.root)
-
-
-# Zapisanie kodu TikZ do pliku
-writeTikzToFile("tree.tex", tikz_code)
+#tikz_code = tikz_tree(avl.root)
+#
+#
+## Zapisanie kodu TikZ do pliku
+#writeTikzToFile("tree.tex", tikz_code)
+#
